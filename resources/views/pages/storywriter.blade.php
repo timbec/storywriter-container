287c651ba3d9982e907bf22c4f0559bd93d1dc57
Tim Beckett <timbeckett@MacBookAir.home>
2025-07-21 13:36:13 -0400

    Added new storywriter build without API Keys

--- resources/views/pages/storywriter.blade.php
+++ resources/views/pages/storywriter.blade.php
@@ -10,7 +10,7 @@
     </button>
 </form>
     <iframe
-        src="{{ config('app.storywriter_url') }}/storywriter"
+        src="{{ config('app.storywriter_url') }}"
         style="width: 100%; height: 100vh; border: 0;"
         allow="clipboard-write"
     ></iframe>
